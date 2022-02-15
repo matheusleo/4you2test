64e13e68a52b316bfc312fccdb514e4357000eff
matheusleo <matheusleotrab@gmail.com>
2022-02-15 15:56:16 -0300

    fix ex11, c02
    modify print order

--- c02-test/ex11.c
+++ c02-test/ex11.c
@@ -3,12 +3,12 @@
 int	main(void)
 {
 	char *word1="Oi\nvoce esta bem?";
-	ft_putstr_non_printable(word1);
 	printf("\nEsperado: Oi\\0avoce esta bem?\n");
+	ft_putstr_non_printable(word1);
 	printf("\n");
 
 	char *word2="Oi\nvoce e\tsta b\vem?";
-	ft_putstr_non_printable(word2);
 	printf("\nEsperado: Oi\\0avoce e\\09sta b\\0bem?\n");
+	ft_putstr_non_printable(word2);
 	printf("\n");
 }
