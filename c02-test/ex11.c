0dd11978b940d3785f8145cfc62349eec8df6440
matheusleo <matheusleotrab@gmail.com>
2022-02-15 10:01:15 -0300

    fix function name from exercise 11, c02-test

--- c02-test/ex11.c
+++ c02-test/ex11.c
@@ -3,12 +3,12 @@
 int	main(void)
 {
 	char *word1="Oi\nvoce esta bem?";
-	ft_putstr_with_non_printable(word1);
+	ft_putstr_non_printable(word1);
 	printf("\nEsperado: Oi\\0avoce esta bem?\n");
 	printf("\n");
 
 	char *word2="Oi\nvoce e\tsta b\vem?";
-	ft_putstr_with_non_printable(word2);
+	ft_putstr_non_printable(word2);
 	printf("\nEsperado: Oi\\0avoce e\\09sta b\\0bem?\n");
 	printf("\n");
 }
