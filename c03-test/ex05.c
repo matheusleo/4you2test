d3537e7c90cc0b1a67560c504ecc10e7fe78e42d
Matheus Leonardo <82543651+matheusleo@users.noreply.github.com>
2022-02-16 10:09:58 -0300

    fix ex05, c03-test
    improve the test showing what is expected and which is the result

--- c03-test/ex05.c
+++ c03-test/ex05.c
@@ -1,4 +1,3 @@
-//NENHUMA SEGURANÇA DE EXATIDÃO É DADA EM RELAÇÃO A ESTE TESTE#include <string.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -7,7 +6,6 @@ int	main(void)
 	char	word1[20] = "matheus";
 	char	*word2 = " leo";
 	char	result1 = ft_strlcat(word1, word2, 10);
-	printf("%d\n", result1);
-	printf("(%s)\n", word1);
-	printf("%s\n", word2);
+	printf("Esperado: 11\nResultado:%d\n", result1);
+	printf("\nEsperado: (matheus l)\nResultado: (%s)\n", word1);
 }
