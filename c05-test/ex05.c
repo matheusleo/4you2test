19840b2a48da812f16a78a8de45a7285b8818b7b
matheusleo <matheusleotrab@gmail.com>
2022-02-18 18:56:40 -0300

    improve c05-test
    modify a test to be possible to see the result is correct when the number is does not have square root

--- c05-test/ex05.c
+++ c05-test/ex05.c
@@ -7,12 +7,12 @@ int	main(void)
 	int result2 = ft_sqrt(16);
 	int result3 = ft_sqrt(81);
 	int result4 = ft_sqrt(625);
-	int result5 = ft_sqrt(390625);
+	int result5 = ft_sqrt(12345690);
 
 	printf("Esperado: 2\nResultado: %d\n\n", result);
 	printf("Esperado: 3\nResultado: %d\n\n", result1);
 	printf("Esperado: 4\nResultado: %d\n\n", result2);
 	printf("Esperado: 9\nResultado: %d\n\n", result3);
 	printf("Esperado: 25\nResultado: %d\n\n", result4);
-	printf("Esperado: 625\nResultado: %d\n", result5);
+	printf("Esperado: 0\nResultado: %d\n", result5);
 }
