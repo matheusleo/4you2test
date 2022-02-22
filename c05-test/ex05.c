f999b5c5cb05052f721f2a368c371c13be93b129
matheusleo <matheusleotrab@gmail.com>
2022-02-21 23:13:06 -0300

    improve the ex-5, c05
    improve the test by adding some edge cases to the test. Eg.: add the biggest int that has a real squareroot and a number bigger than the previous one

--- c05-test/ex05.c
+++ c05-test/ex05.c
@@ -8,6 +8,8 @@ int	main(void)
 	int result3 = ft_sqrt(81);
 	int result4 = ft_sqrt(625);
 	int result5 = ft_sqrt(12345690);
+	int result6 = ft_sqrt(2147395600);
+	int result7 = ft_sqrt(2147395601);
 
 	printf("Esperado: 2\nResultado: %d\n\n", result);
 	printf("Esperado: 3\nResultado: %d\n\n", result1);
@@ -15,4 +17,6 @@ int	main(void)
 	printf("Esperado: 9\nResultado: %d\n\n", result3);
 	printf("Esperado: 25\nResultado: %d\n\n", result4);
 	printf("Esperado: 0\nResultado: %d\n", result5);
+	printf("Esperado: 46340\nResultado: %d\n", result6);
+	printf("Esperado: 0\nResultado: %d\n", result7);
 }
