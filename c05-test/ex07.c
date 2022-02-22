1d0d8e826241b70174065ffa8c99a655c767b74f
matheusleo <matheusleotrab@gmail.com>
2022-02-21 23:07:22 -0300

    fix ex07, c05
    fix the test by modifying some input and some expected results

--- c05-test/ex07.c
+++ c05-test/ex07.c
@@ -3,14 +3,14 @@
 int main(void)
 {
 	int	result1 = ft_find_next_prime(2);
-	int	result2 = ft_find_next_prime(7);
-	int	result3 = ft_find_next_prime(13);
+	int	result2 = ft_find_next_prime(8);
+	int	result3 = ft_find_next_prime(14);
 	int	result4 = ft_find_next_prime(71);
-	int	result5 = ft_find_next_prime(241);
+	int	result5 = ft_find_next_prime(240);
 
-	printf("Esperado: 3\nResultado: %d\n\n", result1);
+	printf("Esperado: 2\nResultado: %d\n\n", result1);
 	printf("Esperado: 11\nResultado: %d\n\n", result2);
 	printf("Esperado: 17\nResultado: %d\n\n", result3);
-	printf("Esperado: 73\nResultado: %d\n\n", result4);
-	printf("Esperado: 251\nResultado: %d\n\n", result5);
+	printf("Esperado: 71\nResultado: %d\n\n", result4);
+	printf("Esperado: 241\nResultado: %d\n\n", result5);
 }
