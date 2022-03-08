ea811ab84ef4ce3f7f271dc8d6fcca6a5b68c507
matheusleo <matheusleotrab@gmail.com>
2022-03-08 00:01:26 -0300

    improve ex11, c02
    improve the message structure to avoid repetetion or necessity to modify the same thing on two different places

--- c02-test/ex11.c
+++ c02-test/ex11.c
@@ -2,13 +2,18 @@
 
 int	main(void)
 {
+	char *expect_msg = "Espera-se :";
+	char *result_msg = "Resulta em: ";
+
 	char *word1="Oi\nvoce esta bem?";
-	printf("\nEsperado: Oi\\0avoce esta bem?\n");
+	printf("\n%s Oi\\0avoce esta bem?\n", expect_msg);
+	write(1, result_msg, 12);
 	ft_putstr_non_printable(word1);
 	printf("\n");
 
 	char *word2="Oi\nvoce e\tsta b\vem?";
-	printf("\nEsperado: Oi\\0avoce e\\09sta b\\0bem?\n");
+	printf("\n%s Oi\\0avoce e\\09sta b\\0bem?\n", expect_msg);
+	write(1, result_msg, 12);
 	ft_putstr_non_printable(word2);
 	printf("\n");
 }
